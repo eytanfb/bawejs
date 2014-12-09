99cdfd8b43b89214d70ff7428da0222b9b93b3bf
Eytan Anjel <anjel@usc.edu>
2014-12-08 21:58:17 -0800

    finished demo of bawejs chapter 4-6

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,47 @@
-var RocknrollcallYeoman = window.RocknrollcallYeoman = Ember.Application.create();
+var RocknrollcallYeoman = window.RocknrollcallYeoman = Ember.Application.create({
+  LOG_TRANSITIONS: true,
+  LOG_ACTIVE_GENERATION: true
+});
+
+Ember.Handlebars.helper('hotttnesss-badge', function(value, options) {
+  var h = parseFloat(value);
+  var hotttnesss_num = Math.round(h * 10);
+  var hotttnesss_css = Math.ceil(h * 10) - 1;
+  var html = "<h4>Hotness: ";
+  if (hotttnesss_num > 0) {
+    html += '<i class="hotttnesss">';
+    for (var i = 0; i < hotttnesss_css; i++) {
+      html += '<i class="glyphicon glyphicon-fire hotttnesss' + i + '"></i>';
+    }
+    html += "</i>";
+    html += '<span class="hotttnesss-badge">' + hotttnesss_css + '</span></h4>';
+  } else {
+    html += "0</h4>";
+  }
+  return new Handlebars.SafeString(html);
+});
+
+RocknrollcallYeoman.ApplicationController = Em.ObjectController.extend({
+  searchTerms: '',
+  applicationName: function(){
+    var st = this.get('searchTerms');
+    if(st){
+      return st + "???";
+    } else {
+      return "Rock 'n Roll Call";
+    }
+  }.property("searchTerms"),
+  actions: {
+    submit: function(){
+      this.transitionToRoute('search-results', this.get("searchTerms"));
+    }
+ }
+});
 
 /* Order and include as you please. */
-require('scripts/controllers/*');
 require('scripts/store');
 require('scripts/models/*');
+require('scripts/controllers/*');
 require('scripts/routes/*');
 require('scripts/components/*');
 require('scripts/views/*');
